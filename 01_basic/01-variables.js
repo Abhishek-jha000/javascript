7ae7e53090a5e2affbb0e9c454fa56b4cd1433a7
Abhishek-jha000 <220101120016@cutm.ac.in>
2024-08-22 10:26:16 +0000

    conversion

---
+++ 01_basic/01-variables.js
@@ -0,0 +1,14 @@
+const accountId = 144533;
+let accountEmail = "abhi@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bihar"
+
+ //accountId = //not allowed
+
+accountEmail = "abhi@gmail.com"
+accountPassword = "125461"
+accountCity = "BIHAR"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict";//treat all js code as never version
+
+//alert(4 + 4)//
+console.log("Abhishek")
+
+
+let name = "Abhisahek"
+let age = 18
+let isLoggedIn = false
+
+
+// 
+console.log(typeof age);
