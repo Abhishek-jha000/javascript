ebbcb08d8ce3a3881f0f974de394da0056fa4235
Abhishek-jha000 <220101120016@cutm.ac.in>
2024-08-26 06:58:25 +0000

    array 2

---
+++ 02_basic/02_arrays.js
@@ -0,0 +1,39 @@
+const marvel_heros =["thor ","ironman","Spiderman"]
+
+    const dc_heros = ["superman", "flash","batmanman"]
+
+
+//marvel_heros.push(dc_heros)
+
+
+//console.log(marvel_heros)
+
+
+
+//marvel_heros.concat(dc_heros)
+//console.log(marvel_heros);
+
+//const allHeros = marvel_heros.concat(dc_heros)
+
+//console.log(allHeros);
+
+//const all_new_heros = [...marvel_heros, ...dc_heros]
+//console.log(all_new_heros);
+
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+const real_another_array = another_array.flat(Infinity)
+//console.log(real_another_array);
+//console.log(Array.isArray("hitesh"));
+//console.log(Array.from("himanshu"))
+console.log(Array.from({name: "Abhishek"}))//intersting
+
+let score1 = 1000
+let score2= 2000
+let score3 =300
+console.log(Array.of (score1,score2,score3));
+
+
+
+
