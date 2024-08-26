f3b4a82f2c56feb75c1abc4e3cf9176c67b1c992
Abhishek-jha000 <220101120016@cutm.ac.in>
2024-08-26 12:55:42 +0000

    obj2nd

---
+++ 02_basic/04_object.js
@@ -0,0 +1,50 @@
+//const tinderUser = new Object()
+const tinderUser = {}
+tinderUser.id = "12332"
+tinderUser.name = "Abhishek"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+ const regularUser ={
+    email:"some@gmail.com",
+    fullname :{
+userfullname:{
+    firstname:"abhishek",
+    lastname:"jha"
+}
+    }
+ }
+ //console.log(regularUser.fullname.userfullname.firstname);
+ 
+ const obj1 = {1: "a", 2: "b"}
+
+ const obj2 =  {3: "a", 4:  "b"}
+
+ const obj4 =   {5:  "a",6:   "b"}       
+ 
+ 
+const obj3 ={...obj1,  ...obj2,}
+//console.log(obj3);
+
+const user = [
+   {
+    id: 1,
+    email:"abhi@gmail.com"
+   },
+   {
+    id: 1,
+    email:"abhi@gmail.com"
+   },
+]
+
+user[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+
+console.log(Object.values(tinderUser));
+
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
