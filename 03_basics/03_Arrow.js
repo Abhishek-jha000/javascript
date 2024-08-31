4a04bbb15400755227929d7adfe80b50770d1547
Abhishek-jha000 <220101120016@cutm.ac.in>
2024-08-31 10:44:17 +0000

    function2

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,29 @@
+const user = {
+username:"Abhishek",
+price:999,
+welcomeMessage: function(){
+    console.log(`${this.username},welcome to website`);
+    console.log(this);
+    
+}
+
+}
+//  user.welcomeMessage()
+//  user.username = "sam"
+//  user.welcomeMessage()
+//console.log(this);
+ function chi(){
+    let username = "hitesh"
+    //console.log(this.username);
+    
+    
+    
+ }
+//chi()
+const addtwo = (num1,num2) => {username:"Abhishek"}
+     num1 + num2 
+
+console.log(addtwo(3,4))
+
+//const  myArry = [2,5,3,7,8]
+//myArry.forEach
