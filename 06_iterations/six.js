7d9ded44a16d0a44117827ea42c4128ea87af2f3
Abhishek-jha000 <220101120016@cutm.ac.in>
2024-09-06 06:39:12 +0000

    reduce map

---
+++ 06_iterations/six.js
@@ -0,0 +1,13 @@
+// const coding = ["js","ruby","java","python","cpp"]
+
+
+// const values = coding.forEach( (item) =>{
+//     console.log(item);
+//     return item
+    
+// })
+// console.log(values);
+
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+//const newNums = myNums.filter( (num) => num > 7)
+//console.log(newNums);
---
+++ 06_iterations/three.js
@@ -0,0 +1,46 @@
+const arr = [1,2,3,4,5]
+//for (const num of arr) {
+  //  console.log(num);
+    
+//}
+//Maps 
+// const map = new Map()
+// map.set('IN',"india")
+// map.set('USA',"UNITED STATES OF AMERICA")
+// map.set('FR',"France")
+
+// map.set('IN',"india")
+
+//console.log(map);
+//for (const [key ,value] of map){
+  //  console.log(key,':-',value);
+
+
+//}
+const myObject = {
+    'game1': 'NFS',
+    'game2':    'spiderman'
+}
+// for (const [key ,value] of myObject)
+//     console.log(key,':-',value);
+
+ const mycoding =[
+{
+  languageName: "javascript",
+  languagefileName: "js"
+},
+{
+  languageName: "java",
+  languagefileName: "java"
+},
+{
+  languageName: "python",
+  languagefileName: "py"
+}
+
+]
+mycoding.forEach( (item) => {
+console.log(item.languageName);
+
+} )
+    
